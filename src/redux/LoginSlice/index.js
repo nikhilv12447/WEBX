1337d92a9723b4f11657cc5d6c3460796a2eab3c
Nikhil Verma <nikhil.verma12447@gmail.com>
2025-03-07 12:24:42 +0530

    responsive done

--- src/redux/LoginSlice/index.js
+++ src/redux/LoginSlice/index.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const loginSlice = createSlice({
     name: 'login',
     initialState: {
-        isLogin: true
+        isLogin: false
     },
     reducers: {
         setLogin: (state, { payload }) => {
